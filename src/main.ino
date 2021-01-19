77f72e6a8565fa5daa5ccbabcb49159e629c7e39
Channarong Teanram <channarong.te@cdg.co.th>
2021-01-19 14:39:02 +0700

    refector

--- src/main.ino
+++ src/main.ino
@@ -10,23 +10,39 @@
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// array of P5ID's to print values of
-const int PIDS[] = {
-  CALCULATED_ENGINE_LOAD,
-  ENGINE_COOLANT_TEMPERATURE,
-  ENGINE_RPM,
-  VEHICLE_SPEED,
-  AIR_INTAKE_TEMPERATURE,
-  MAF_AIR_FLOW_RATE,
-  THROTTLE_POSITION,
-  RUN_TIME_SINCE_ENGINE_START,
-  FUEL_TANK_LEVEL_INPUT,
-  ABSOLULTE_BAROMETRIC_PRESSURE,
-  ABSOLUTE_LOAD_VALUE,
-  RELATIVE_THROTTLE_POSITION
+
+byte battertyIcon[] = {
+  0x10,
+  0x18,
+  0x18,
+  0x1F,
+  0x1F,
+  0x03,
+  0x03,
+  0x01
 };
 
-const int NUM_PIDS = sizeof(PIDS) / sizeof(PIDS[0]);
+byte ectIcon[] = {
+  0x0E,
+  0x08,
+  0x0E,
+  0x08,
+  0x0E,
+  0x08,
+  0x0C,
+  0x0C
+};
+
+byte tftIcon[] = {
+  0x00,
+  0x00,
+  0x04,
+  0x0E,
+  0x1B,
+  0x0E,
+  0x04,
+  0x00
+};
 
 
 void setup() {
@@ -36,6 +52,9 @@ void setup() {
     // init done
 
     lcd.begin();
+    lcd.createChar(0, battertyIcon);
+    lcd.createChar(1, ectIcon);
+    lcd.createChar(2, tftIcon);
 
     // Turn on the blacklight and print a message.
     lcd.backlight();
@@ -57,24 +76,19 @@ void setup() {
 
 
 void loop() {
-    char *pidName[4]={'\0'};
+    char *pidName[3]={'\0'};
     pidName[0]=(char*)"ECT";
     pidName[1]=(char*)"TFT";
-    pidName[2]=(char*)"RPM";
-    pidName[3]=(char*)"EOT";
+    pidName[2]=(char*)"BAT";
 
-    float pidValue[4] = {};
+    float pidValue[3] = {};
     pidValue[0] = 0;
     pidValue[1] = 0;
     pidValue[2] = 0;
-    pidValue[3] = 0;
 
     float TFT = OBD2.pidReadExtra(0x1E,0x1C);
     float ECT = OBD2.pidRead(0x05);
-    float RPM = OBD2.pidRead(0x0C);
-    // float AIT = OBD2.pidRead(0x0F);
-    // float MAF = OBD2.pidRead(0x10);
-    float CMV = OBD2.pidRead(0x42);
+    float BAT = OBD2.pidRead(0x42);
 
     pidName[0]=(char*)"ECT";
     pidValue[0] = ECT;
@@ -82,11 +96,8 @@ void loop() {
     pidName[1]=(char*)"TFT";
     pidValue[1] = TFT;
 
-    pidName[2]=(char*)"RPM";
-    pidValue[2] = RPM;
-
-    pidName[3]=(char*)"CMV";
-    pidValue[3] = CMV;
+    pidName[2]=(char*)"BAT";
+    pidValue[2] = BAT;
    
     displayGauge(pidName,pidValue);   
 
@@ -97,18 +108,22 @@ void loop() {
 void displayGauge(char **pidName, float pidValue[]) {
 
   char valueTemp[10];
-  int col = 0;
-  int row = 0;
-  for (int i = 1; i <= 4; i++) {
 
-    if(i > 2) col = 9;
-    row = i % 2;
-    lcd.setCursor(col, row);
-    lcd.print(String(pidName[i-1]));
-    lcd.setCursor(col + 4, row);
-    sprintf(valueTemp,"%f",pidValue[i-1]);
+  for (int i = 0; i < 3; i++) {
+    //Text
+    lcd.setCursor((i*5), 0);
+    lcd.print(String(pidName[i]));
+
+    //icon
+    lcd.setCursor(((i*5)+3), 0);
+    lcd.write(i);
+
+    //value
+    lcd.setCursor((i*5), 1);
+    //pidValue[i]
+    sprintf(valueTemp,"%2.2f",(double)pidValue[i]);
     lcd.print(valueTemp); 
-   
   }
+  
 
 }
